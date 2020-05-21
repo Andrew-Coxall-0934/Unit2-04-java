c27fb1d90749419419d5c6847f58114ca52bbb5c
Ubuntu <ubuntu@ip-172-31-46-83.ec2.internal>
2020-05-21 18:43:05 +0000

    commit message

---
+++ constants.py
@@ -0,0 +1,11 @@
+# Copyright (c) Ac industries All rights reserved  
+#
+# Created by: Andrew Coxall
+# Created on: May 21, 2020
+# Constants for Cost of a pizza calculator
+
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
--- pizza_calculator.py
+++ pizza_calculator.py
@@ -1,5 +1,24 @@
-# Copyright (c) Ac industries All rights reserved  
+# Copyright (c) Ac industries All rights reserved
 #
 # Created by: Andrew Coxall
 # Created on: May 21, 2020
 # Cost of a pizza calculator
+
+import constants
+
+
+def main():
+    diameter = int(input("Enter the diameter of the pizza you would" +
+                         "like (inch): "))
+
+    sub_total = constants.LABOR + constants.RENT + \
+        (diameter * constants.COST_PER_INCH)
+    total = sub_total + (sub_total * constants.HST)
+
+    print("")
+    print("The cost for a {0} inch pizza is: ${1:,.2f}"
+          .format(diameter, total))
+
+
+if __name__ == "__main__":
+    main()
